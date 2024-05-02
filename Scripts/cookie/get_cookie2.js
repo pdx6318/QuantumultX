632500d81ef8e1a5ba3c1ff3bab073eac5932306
pdx6318 <160988662+pdx6318@users.noreply.github.com>
2024-05-02 12:19:47 +0800

    Update get_cookie2.js

--- Scripts/cookie/get_cookie2.js
+++ Scripts/cookie/get_cookie2.js
@@ -48,9 +48,12 @@ if (req_url.includes("myy2.com/h5/17/userCenter")) {
      cookieValue = req_headers["Cookie"];
     const token = cookieValue;
     console.log("获取到token：" + token);
-    
+    if (token !== "undefined") {
     $.write(token, '#pdx_vip_token2');
     $.notify('VIPtoken 获取成功✅', '', token);
+} else {
+    console.log("获取的token为空，不执行特定代码");
+  }
   }
 }
 // 将数据字符串解析为对象
